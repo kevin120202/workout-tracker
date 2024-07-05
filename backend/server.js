ba9d00d2704e806120088366b98b9ff84f0cc283
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-05 15:39:19 -0500

    added routes

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,10 @@
 const express = require("express")
 const app = express()
+
+const PORT = process.env.PORT || 4000
+
+app.get("/", (req, res) => {
+    res.json({ msg: "welcome to the app" })
+})
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`))
